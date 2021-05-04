83da5c8309d0e9c22a3a995580395cb88d9ee912
AlBit7 <72737404+AlBit7@users.noreply.github.com>
2021-05-04 17:44:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
+const soluzione = randomizza();
+
 var coloreSelezionato = 0;
 var tentativi = 10;
 
 p = new Array(-1, -1, -1, -1);
 
 const yellow = document.getElementById('yellow');
-const green = document.getElementById('green');
-const blue = document.getElementById('blue');
-const red = document.getElementById('red');
-const white = document.getElementById('white');
-const black = document.getElementById('black');
+const green  = document.getElementById('green');
+const blue   = document.getElementById('blue');
+const red    = document.getElementById('red');
+const white  = document.getElementById('white');
+const black  = document.getElementById('black');
 
-const s1 = document.getElementById('s1');
-const s2 = document.getElementById('s2');
-const s3 = document.getElementById('s3');
-const s4 = document.getElementById('s4');
+const s1     = document.getElementById('s1');
+const s2     = document.getElementById('s2');
+const s3     = document.getElementById('s3');
+const s4     = document.getElementById('s4');
 
-const label = document.getElementById('coloreSelezionato');
-const vai = document.getElementById('vai');
+const label  = document.getElementById('coloreSelezionato');
+const vai    = document.getElementById('vai');
 
 function convertiInColore(colore) {
     switch (colore) {
@@ -31,12 +33,24 @@ function convertiInColore(colore) {
     }
 }
 
+function randomizza() {
+
+    var sequenza = new Array(4);
+
+    for (let i = 0; i < 4; i++) {
+        sequenza[i] = Math.floor(Math.random() * (5 - 0) + 0);
+    }
+
+    return sequenza;
+
+}
+
 function controlla(numeriComputer, numeriUtente) {
 
     console.log('soluzione: ' + numeriComputer);
     console.log('tentativo: ' + numeriUtente);
 
-    let soluzione = numeriComputer;
+    let soluzione = numeriComputer.slice(0);
     let pallini = new Array(0, 0);
 
     // pallini[0] --> bianchi
@@ -53,84 +67,49 @@ function controlla(numeriComputer, numeriUtente) {
         for (let j = 0; j < numeriUtente.length; j++) {
             if (i != j && soluzione[i] == numeriUtente[j]) {
                 pallini[1]++;
+                break;
             }
         }
     }
+
+    console.log('pallini bianchi-neri: ' + pallini[0] + '-' + pallini[1]);
 
     return pallini;
 
 }
 
-yellow.addEventListener('click', function () {
-    coloreSelezionato = 0;
-    label.innerHTML = "colore selezionato: giallo";
-});
-green.addEventListener('click', function () {
-    coloreSelezionato = 1;
-    label.innerHTML = "colore selezionato: verde";
-});
-blue.addEventListener('click', function () {
-    coloreSelezionato = 2;
-    label.innerHTML = "colore selezionato: blu";
-});
-red.addEventListener('click', function () {
-    coloreSelezionato = 3;
-    label.innerHTML = "colore selezionato: rosso";
-});
-white.addEventListener('click', function () {
-    coloreSelezionato = 4;
-    label.innerHTML = "colore selezionato: bianco";
-});
-black.addEventListener('click', function () {
-    coloreSelezionato = 5;
-    label.innerHTML = "colore selezionato: nero";
-});
-
-s1.addEventListener('click', function () {
-    p[0] = coloreSelezionato;
-    s1.style.backgroundColor = convertiInColore(coloreSelezionato);
-});
-s2.addEventListener('click', function () {
-    p[1] = coloreSelezionato;
-    s2.style.backgroundColor = convertiInColore(coloreSelezionato);
-});
-s3.addEventListener('click', function () {
-    p[2] = coloreSelezionato;
-    s3.style.backgroundColor = convertiInColore(coloreSelezionato);
-});
-s4.addEventListener('click', function () {
-    p[3] = coloreSelezionato;
-    s4.style.backgroundColor = convertiInColore(coloreSelezionato);
-});
-
 vai.addEventListener('click', function () {
+
+    // sposta i colori nella tabella degli storici
 
     const t1 = document.getElementById('t' + tentativi + '-1');
     const t2 = document.getElementById('t' + tentativi + '-2');
     const t3 = document.getElementById('t' + tentativi + '-3');
     const t4 = document.getElementById('t' + tentativi + '-4');
 
-    const esitoPallini = new Array(document.getElementById('p' + tentativi + '-1'), document.getElementById('p' + tentativi + '-2'), document.getElementById('p' + tentativi + '-3'), document.getElementById('p' + tentativi + '-4'));
-
     t1.style.backgroundColor = convertiInColore(p[0]);
     t2.style.backgroundColor = convertiInColore(p[1]);
     t3.style.backgroundColor = convertiInColore(p[2]);
     t4.style.backgroundColor = convertiInColore(p[3]);
 
-    const soluzione = new Array(4, 0, 3, 2);
+    // assegnamento pirulini bianchi e neri
+
+    const esitoPallini = new Array(document.getElementById('p' + tentativi + '-1'), document.getElementById('p' + tentativi + '-2'), document.getElementById('p' + tentativi + '-3'), document.getElementById('p' + tentativi + '-4'));
+
     pallini = controlla(soluzione, p)
+    if (pallini[0] == 4) {
+        label.innerHTML = "INDOVINATO!!";
+    }
 
     for (let i = 0; i < pallini[0]; i++) {
         esitoPallini[i].style.backgroundColor = "white";
     }
-    
-    if (pallini[0] == 4) {
-        label.innerHTML = "INDOVINATO!!";
-    }
-    
+
     for (let i = 0; i < pallini[1]; i++) {
-        esitoPallini[3-i].style.backgroundColor = "black";
+        esitoPallini[3 - i].style.backgroundColor = "black";
     }
+
+    // resetta tutto per il prossimo tentativo
 
     p[0] = p[1] = p[2] = p[3] = coloreSelezionato = -1;
     s1.style.backgroundColor = convertiInColore(coloreSelezionato);
@@ -141,5 +120,60 @@ vai.addEventListener('click', function () {
     tentativi--;
 
     vai.innerHTML = tentativi + ' tentativi';
+
+});
+
+document.getElementById('body').addEventListener('click', function (event) {
+
+    switch (event.target.id) {
+        case "yellow":
+            coloreSelezionato = 0;
+            label.innerHTML = "colore selezionato: giallo";
+            break;
+
+        case "green":
+            coloreSelezionato = 1;
+            label.innerHTML = "colore selezionato: verde";
+            break;
+
+        case "blue":
+            coloreSelezionato = 2;
+            label.innerHTML = "colore selezionato: blu";
+            break;
+
+        case "red":
+            coloreSelezionato = 3;
+            label.innerHTML = "colore selezionato: rosso";
+            break;
+
+        case "white":
+            coloreSelezionato = 4;
+            label.innerHTML = "colore selezionato: bianco";
+            break;
+
+        case "black":
+            coloreSelezionato = 5;
+            label.innerHTML = "colore selezionato: nero";
+            break;
+
+        case "s1":
+            p[0] = coloreSelezionato;
+            s1.style.backgroundColor = convertiInColore(coloreSelezionato); break;
+
+        case "s2":
+            p[1] = coloreSelezionato;
+            s2.style.backgroundColor = convertiInColore(coloreSelezionato); break;
+
+        case "s3":
+            p[2] = coloreSelezionato;
+            s3.style.backgroundColor = convertiInColore(coloreSelezionato); break;
+
+        case "s4":
+            p[3] = coloreSelezionato;
+            s4.style.backgroundColor = convertiInColore(coloreSelezionato); break;
+
+        default:
+            break;
+    }
 
 });
